04cf7d0bf711339e3525ac4fb9d77ef86b26620e
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-05-18 15:07:37 +0300

    labs done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
+require "pry"
 class Dog
+  #setter method
+  def name=(name)
+    @name=name
+  end
+
+  #getter method
+  def name
+    @name
+  end
+  #breed setter method
+  def breed=(breed)
+    @breed = breed
+  end
+  #breed getter method
+  def breed
+    @breed
+  end
 
 end
+
+dog1= Dog.new
+dog1.name=("Pope")
+dog1.breed=("Corgi")
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
+#person setter method
+  def name=(name)
+    @name=name
+  end
 
+#person getter method
+def name
+  @name
 end
+
+#job setter method
+def job=(job)
+  @job=job
+end
+#job getter method
+def job
+  @job
+end
+end
+
+person1=Person.new
+person1.name="Feisal"
+person1.job="Enginner"
+
+
+
